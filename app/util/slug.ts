371723947b3e323e9874333d99c4a6b9f4ce046d
Sean Middleditch <sean@seanmiddleditch.com>
2017-08-31 21:42:22 -0700

    Replace ShowDown with markdown-it

--- app/util/slug.ts
+++ app/util/slug.ts
@@ -1,4 +1,4 @@
 export function sanitizeSlug(slug: string) : string
 {
-    return slug.replace(/[^a-zA-Z0-9-]+/g, '-').substring(0, 32).toLowerCase();
+    return slug.replace(/[^a-zA-Z0-9-]+/g, ' ').trim().substring(0, 32).trim().toLowerCase().replace(' ', '-');
 }
--- static/scripts/markup.jsx
+++ static/scripts/markup.jsx
@@ -1,34 +1,53 @@
-showdown.extension('wiki', {
-    type: 'lang',
-    regex: /\[\[([^\]]+)\]\]/g,
-    replace: '<a href="/n/$1">$1</a>'
-});
-
 class RenderMarkup extends React.Component
 {
     constructor(props)
     {
         super(props);
 
-        const engine = this.engine = new showdown.Converter({extensions: ['wiki']});
-        engine.setFlavor('github');
-        engine.setOption('omitExtraWLInCodeBlocks', 'true');
-        engine.setOption('ghCompatibleHeaderId', 'true');
-        engine.setOption('simplifiedAutoLink', 'true');
-        engine.setOption('excludeTrailingPunctuationFromURLs', 'true');
-        engine.setOption('literalMidWordUnderscores', 'true');
-        engine.setOption('strikethrough', 'true');
-        engine.setOption('tables', 'true');
-        engine.setOption('ghCodeBlocks', 'true');
-        engine.setOption('tasklists', 'true');
-        engine.setOption('simpleLineBreaks', 'true');
-        engine.setOption('disableForced4SpacesIndentedSublists', 'true');
-        engine.setOption('parseImgDimensions', 'true');
+        this.md = new markdownit({
+            html: true,
+            breaks: true,
+            linkify: true,
+            typographer: true
+        }).use(this.wikiPlugin);
+    }
+
+    wikiPlugin(md)
+    {
+        const id = 'WIKI_LINK';
+        md.inline.ruler.push(id, (state, silent) => {
+            const startPos = state.src.indexOf('[[', state.pos);
+            if (startPos != -1)
+            {
+                const endPos = state.src.indexOf(']]', startPos + 2);
+                if (endPos != -1)
+                {
+                    const content = state.src.slice(startPos + 2, endPos);
+                    console.log(content);
+                    
+                    const token = state.push(id, '', 0);
+                    state.pos += 4 + content.length;
+
+                    const [slug, title] = content.split('|', 2).map(s => s.trim());
+                    token.meta = {slug, title: title && title.length ? title : slug}
+                    
+                    return true;
+                }
+            }
+            return false;
+        });
+
+        md.renderer.rules[id] = (tokens, id, options, env) => {
+            const token = tokens[id];
+            const slug = token.meta.slug.replace(/[^a-zA-Z0-9]+/, ' ').trim().replace(' ', '-');
+            const title = token.meta.title.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;');
+            return '<a href="/n/' + slug + '">' + title + '</a>';
+        };
     }
 
     render()
     {
-        const html = this.engine.makeHtml(this.props.markup);
+        const html = this.md.render(this.props.markup);
         return <div dangerouslySetInnerHTML={{__html: html}}/>
     }
 }
