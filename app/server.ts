3452e08da4a31eb89d00927b48a47cc7e1c974fa
Sean Middleditch <sean@seanmiddleditch.com>
2017-09-13 00:05:28 -0700

    Update configuration for better Heroku compatibility, including Redis store for sessions

--- app/server.ts
+++ app/server.ts
@@ -6,17 +6,48 @@ import * as BodyParser from 'body-parser';
 import * as squell from 'squell';
 import * as path from 'path';
 import * as passport from 'passport';
+import * as redis from 'connect-redis';
+import * as fs from 'fs';
 
 import {AuthRouter, LabelRouter, NoteRouter} from './routes';
 import {LibraryModel, LabelModel, NoteModel, UserModel} from './models';
 import GoogleAuth from './auth/GoogleAuth';
 
+class Config
+{
+    readonly publicURL: string;
+    readonly googleClientID: string;
+    readonly googleAuthSecret: string;
+    readonly sessionSecret: string;
+    readonly port: number;
+    readonly redisURL: string;
+    readonly databaseURL: string;
+    readonly production: boolean;
+
+    constructor()
+    {
+        this.publicURL = process.env.PUBLIC_URL || 'http://localhost';
+        this.port = parseInt(process.env.PORT, 10);
+        if (this.port != 80)
+            this.publicURL += `:${this.port}`;
+        
+        this.googleClientID = process.env.GOOGLE_CLIENT_ID;
+        this.googleAuthSecret = process.env.GOOGLE_AUTH_SECRET;
+        this.sessionSecret = process.env.CM_SESSION_SECRET;
+        this.redisURL = process.env.REDIS_URL;
+        this.databaseURL = process.env.DATABASE_URL;
+        this.production = process.env.NODE_ENV === 'production';
+    }
+};
+
 (async () => {
     
     const root = path.join(__dirname, '..', '..');
     const staticRoot = path.join(root, 'static');
+
+    const config = new Config();
     
-    const db = new squell.Database(process.env.DATABASE_URL, {dialect: 'postgres', define: {timestamps: false}});
+    const db = new squell.Database(config.databaseURL, {dialect: 'postgres', define: {timestamps: false}});
     
     db.define(LibraryModel);
     db.define(LabelModel);
@@ -25,21 +56,28 @@ import GoogleAuth from './auth/GoogleAuth';
 
     await db.sync();
 
-    const publicPort = process.env.PORT || 8080;
-    const publicURL = process.env.PUBLIC_URL || ('http://localhost:' + publicPort);
+    const RedisStore = redis(session);
 
-    passport.use(GoogleAuth(db, publicURL, process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_AUTH_SECRET));
-    passport.serializeUser((user: UserModel, done) => done(null, user.id));
-    passport.deserializeUser((userID: number, done) => db.query(UserModel).where(m => m.id.eq(userID)).findOne().then(user => done(null, user)).catch(err => done(err)));
+    if (config.googleClientID)
+    {
+        passport.use(GoogleAuth(db, config.publicURL, config.googleClientID, config.googleAuthSecret));
+        passport.serializeUser((user: UserModel, done) => done(null, user.id));
+        passport.deserializeUser((userID: number, done) => db.query(UserModel).where(m => m.id.eq(userID)).findOne().then(user => done(null, user)).catch(err => done(err)));
+    }
 
     const app = express();
     app.use(BodyParser.urlencoded({extended: false}));
     app.use(BodyParser.json());
-    app.use(session({secret: process.env.CM_SESSION_SECRET}));
+    app.use(session({
+        secret: config.sessionSecret,
+        resave: false,
+        saveUninitialized: false,
+        store: config.redisURL ? new RedisStore({url: config.redisURL}) : null
+    }));
     app.use(passport.initialize());
     app.use(passport.session());
 
-    if (process.env.NODE_ENV !== 'production')
+    if (config.production)
     {
         console.log('Enabling WebPack development middlware');
 
@@ -54,7 +92,7 @@ import GoogleAuth from './auth/GoogleAuth';
     app.get('/config.js', (req, res) => {
         res.json({
             google: {
-                clientId: process.env.GOOGLE_CLIENT_ID
+                clientId: config.googleClientID
             },
             session: {
                 key: req.sessionID,
@@ -67,13 +105,9 @@ import GoogleAuth from './auth/GoogleAuth';
     app.use(NoteRouter(db));
     app.use(LabelRouter(db));
 
-    // production users should be using a reverse proxy
-    if (process.env.NODE_ENV !== 'production')
-    {
-        app.use(express.static(staticRoot));
-        app.get('*', (req, res) => res.sendFile(path.join(staticRoot, 'index.html')));
-    }
+    app.use(express.static(staticRoot));
+    app.get('*', (req, res) => res.sendFile(path.join(staticRoot, 'index.html')));
 
-    const server = await app.listen(publicPort);
+    const server = await app.listen(config.port);
     console.log(`Listening on port ${server.address().port}`);
 })();
