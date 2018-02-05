55786304dc26aa4681b6d39de96cb896be275db0
Sean Middleditch <sean@seanmiddleditch.com>
2018-02-04 22:32:55 -0800

    Separate the image upload from files to a helper
    Needed for maps and eventually portraits

--- app/routes/campaign/files.ts
+++ app/routes/campaign/files.ts
@@ -1,31 +1,21 @@
-import {S3, AWSError} from 'aws-sdk'
 import {Request, Response} from 'express'
 import PromiseRouter = require('express-promise-router')
 import * as express from 'express'
-import {CampaignModel, MediaModel, MediaStorageModel, MediaFileRepository} from '../../models'
+import {CampaignModel, MediaModel, MediaFileRepository} from '../../models'
 import {checkAccess} from '../../auth'
 import {connection} from '../../db'
-import {config} from '../../config'
 import * as path from 'path'
 import {QueryFailedError} from 'typeorm'
 import * as mime from 'mime'
-import * as fileType from 'file-type'
-import * as imageSize from 'image-size'
 import {URL} from 'url'
 import * as multer from 'multer'
-import * as crypto from 'crypto'
+import {insertMedia} from '../../util/insert-media'
 
 export function files()
 {
     const router = PromiseRouter()
 
     const mediaRepository = connection().getCustomRepository(MediaFileRepository)
-    const storageRepository = connection().getRepository(MediaStorageModel)
-
-    const s3 = new S3()
-    s3.config.region = config.awsRegion
-    s3.config.accessKeyId = config.awsAccessKey
-    s3.config.secretAccessKey = config.awsAuthSecret
 
     router.post('/files', multer({limits: {fileSize: 5*1024*1024}}).single('file'), async (req, res, next) => {
         if (!req.campaign)
@@ -54,12 +44,8 @@ export function files()
             return
         }
 
-        const hashBuffer = crypto.createHash('md5').update(file.buffer).digest()
-        const hashHexMD5 = hashBuffer.toString('hex')
-        const hashBase64MD5 = hashBuffer.toString('base64')
-
-        const contentType = fileType(file.buffer).mime
-        
+        const {storageId, hashHexMD5, contentType} = await insertMedia(file.buffer)
+       
         const extension = path.extname(filePath)
         if (mime.getType(extension) !== contentType)
         {
@@ -67,31 +53,11 @@ export function files()
             return
         }
 
-        const imageInfo = imageSize(file.buffer)
-
-        // find existing storage or create it if necessary
-        let storage = await storageRepository.createQueryBuilder('storage')
-            .select(['id'])
-            .where('content_md5=:md5', {md5: hashHexMD5})
-            .printSql()
-            .getRawOne()
-        if (!storage)
-        {
-            storage = storageRepository.create({
-                contentMD5: hashHexMD5,
-                extension,
-                byteLength: file.size,
-                imageWidth: imageInfo.width,
-                imageHeight: imageInfo.height,
-            })
-            storage = await storageRepository.save(storage)
-        }
-
         // try create a new media entry
         let media = mediaRepository.create({
             path: filePath,
             caption,
-            storageId: storage.id,
+            storageId: storageId,
             campaignId: req.campaign.id,
             attribution: ''
         })
@@ -111,31 +77,6 @@ export function files()
             {
                 throw error
             }
-        }
-
-        // generate a signed URL for the client to upload the full image
-        const putParams: S3.PutObjectRequest = {
-            Bucket: config.s3Bucket,
-            Key: `media/${hashHexMD5}${extension}`,
-            ContentType: contentType,
-            ContentLength: file.size,
-            ContentMD5: hashBase64MD5,
-            ACL: 'public-read',
-            Body: file.buffer
-        }
-        try
-        {
-            const result = await new Promise<S3.PutObjectOutput>((resolve, reject) => {
-                s3.putObject(putParams, (err, data) => {
-                    if (err) reject(err)
-                    else resolve(data)
-                })
-            })
-        }
-        catch (error)
-        {
-            mediaRepository.delete(media)
-            throw error
         }
 
         res.json({
---
+++ app/util/insert-media.ts
@@ -0,0 +1,60 @@
+import {S3, AWSError} from 'aws-sdk'
+import {MediaStorageModel} from '../models'
+import {connection} from '../db'
+import {config} from '../config'
+import * as imageSize from 'image-size'
+import * as fileType from 'file-type'
+import * as crypto from 'crypto'
+
+export async function insertMedia(file: Buffer)
+{
+    const storageRepository = connection().getRepository(MediaStorageModel)
+
+    const hashBuffer = crypto.createHash('md5').update(file).digest()
+    const hashHexMD5 = hashBuffer.toString('hex')
+
+    // find existing storage or create it if necessary
+    const exists = !!await storageRepository.createQueryBuilder('storage')
+        .select(['id'])
+        .where('content_md5=:md5', {md5: hashHexMD5})
+        .getRawOne()
+    if (exists)
+    {
+    }
+
+    const imageInfo = imageSize(file)
+    const contentType = fileType(file).mime
+
+    const storage = storageRepository.create({
+        contentMD5: hashHexMD5,
+        byteLength: file.byteLength,
+        extension: imageInfo.type,
+        imageWidth: imageInfo.width,
+        imageHeight: imageInfo.height,
+    })
+    await storageRepository.save(storage)
+    
+    const s3 = new S3()
+    s3.config.region = config.awsRegion
+    s3.config.accessKeyId = config.awsAccessKey
+    s3.config.secretAccessKey = config.awsAuthSecret
+
+    // generate a signed URL for the client to upload the full image
+    const putParams: S3.PutObjectRequest = {
+        Bucket: config.s3Bucket,
+        Key: `media/${hashHexMD5}${imageInfo.type}`,
+        ContentType: contentType,
+        ContentLength: file.byteLength,
+        ContentMD5: hashBuffer.toString('base64'),
+        ACL: 'public-read',
+        Body: file.buffer
+    }
+    const result = await new Promise<S3.PutObjectOutput>((resolve, reject) => {
+        s3.putObject(putParams, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
+
+    return {hashHexMD5, storageId: storage.id, contentType}
+}
